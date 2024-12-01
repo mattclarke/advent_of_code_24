c46ba9125baa0dc1dbadbbecee8f67aad9eee411
Matt Clarke <matt.clarke@ess.eu>
2024-12-01 07:07:17 +0100

    day 1 done

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 https://adventofcode.com/2024
 
 ## Day 1
-- Part 1: .
-- Part 2: .
+- Part 1: Sort the numbers then find the difference.
+- Part 2: Count the number of occurances.
 
+## Day 2
--- day_1/solver.py
+++ day_1/solver.py
@@ -8,17 +8,30 @@ with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
+
+
+a = []
+b = []
+
+for l in lines:
+    f, s = l.split("   ")
+    a.append(int(f))
+    b.append(int(s))
+
+a.sort()
+b.sort()
 
 result = 0
 
-for l in lines:
-    pass
+for f, s in zip(a, b):
+    result += abs(f - s)
 
-# Part 1 = 
+# Part 1 = 2430334
 print(f"answer = {result}")
 
 result = 0
+for f in a:
+    result += b.count(f) * f
 
-# Part 2 = 
+# Part 2 = 28786472
 print(f"answer = {result}")
