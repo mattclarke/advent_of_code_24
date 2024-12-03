3107f0b29b1adfbec60a1cf9c60b9f1c04c892fd
Matt Clarke <matt.clarke@ess.eu>
2024-12-03 17:14:05 +0100

    do it in one line

--- day_3/solver.py
+++ day_3/solver.py
@@ -15,7 +15,7 @@ result = 0
 
 for l in lines:
     m = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", l)
-    for (a, b) in m:
+    for a, b in m:
         result += int(a) * int(b)
 
 # Part 1 = 174960292
@@ -24,16 +24,16 @@ print(f"answer = {result}")
 result = 0
 do = True
 
-for l in lines:
-    m = re.findall(r"(do)\(\)|(don't)\(\)|mul\((\d{1,3}),(\d{1,3})\)", l)
-    for (d, dn, a, b) in m:
-        if d:
-            do = True
-        elif dn:
-            do = False
-        elif do:
-            result += int(a) * int(b)
+# Insert | between the lines to avoid a line ending with do and the next starting with n't()
+m = re.findall(r"(do)\(\)|(don't)\(\)|mul\((\d{1,3}),(\d{1,3})\)", "|".join(lines))
 
+for d, dn, a, b in m:
+    if d:
+        do = True
+    elif dn:
+        do = False
+    elif do:
+        result += int(a) * int(b)
 
 # Part 2 = 56275602
 print(f"answer = {result}")
