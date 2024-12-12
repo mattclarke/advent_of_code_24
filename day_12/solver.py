81d3da0fdfb3da6ae4cdce594b861cbc88094526
Matt Clarke <matt.clarke@ess.eu>
2024-12-12 07:26:35 +0100

    12: part 1

--- day_12/solver.py
+++ day_12/solver.py
@@ -8,17 +8,57 @@ with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
+layout = {}
+
+for r, line in enumerate(lines):
+    for c, ch in enumerate(line):
+        layout[r, c] = ch
+
+seen = set()
+regions = {}
+count = 0
+
+for r in range(len(lines)):
+    for c in range(len(lines[0])):
+        if (r, c) in seen:
+            continue
+        region = {(r, c)}
+        name = layout[r, c]
+        q = [(r, c)]
+        while q:
+            curr = q.pop(0)
+            for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                nr = curr[0] + dr
+                nc = curr[1] + dc
+                if (nr, nc) in seen:
+                    continue
+                if layout.get((nr, nc)) != name:
+                    continue
+                region.add((nr, nc))
+                q.append((nr, nc))
+                seen.add((nr, nc))
+        regions[count] = region
+        count += 1
+
+
+edges = {}
+for k, v in regions.items():
+    nedges = 0
+    for r, c in v:
+        for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+            nr = r + dr
+            nc = c + dc
+            if (nr, nc) not in v:
+                nedges += 1
+    edges[k] = nedges
 
 result = 0
-
-for l in lines:
-    pass
-
-# Part 1 = 
+for k, v in regions.items():
+    result += len(v) * edges[k]
+# Part 1 = 1465112
 print(f"answer = {result}")
 
 result = 0
 
-# Part 2 = 
+# Part 2 =
 print(f"answer = {result}")
