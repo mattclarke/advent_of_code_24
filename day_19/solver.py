66375dc52b3a03765246e8c40d94cef9d7b440d2
Matt Clarke <matt.clarke@ess.eu>
2024-12-19 07:21:19 +0100

    Day 19: part 1

--- day_19/solver.py
+++ day_19/solver.py
@@ -8,18 +8,52 @@ FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
-lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
+lines = [line.strip() for line in PUZZLE_INPUT.split("\n\n") if line]
+towels = lines[0].split(", ")
+patterns = lines[1].split("\n")
 
 result = 0
 
-for line in lines:
-    pass
+for p in patterns:
+    Q = deque([""])
+    seen = set()
+
+    while Q:
+        pb = Q.popleft()
+        if pb == p:
+            result += 1
+            break
+        if not p.startswith(pb):
+            continue
+        if pb in seen:
+            continue
+        seen.add(pb)
+        for t in towels:
+            Q.append(pb + t)
+
 
 # Part 1 = 
 print(f"answer = {result}")
 
 result = 0
+
+for p in patterns:
+    Q = deque([""])
+    seen = set()
+
+    while Q:
+        pb = Q.popleft()
+        if pb == p:
+            result += 1
+            break
+        if not p.startswith(pb):
+            continue
+        if pb in seen:
+            continue
+        seen.add(pb)
+        for t in towels:
+            Q.append(pb + t)
+
 
 # Part 2 = 
 print(f"answer = {result}")
