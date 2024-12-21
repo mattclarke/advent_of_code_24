7dd9f1a6952416735e15ccaa347b6d9665371269
Matt Clarke <matt.clarke@ess.eu>
2024-12-21 14:05:03 +0100

    21: part 1 done

--- day_21/solver.py
+++ day_21/solver.py
@@ -12,86 +12,113 @@ lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
 print(lines)
 
 num_pad = {
-    "7":(0, 0) ,
-   "8":(0, 1),
-   "9":(0, 2),
-   "4":(1, 0),
-   "5":(1, 1),
-   "6":(1, 2),
-   "1":(2, 0),
-   "2":(2, 1),
-   "3":(2, 2),
-   "":(3, 0) ,
-   "0":(3, 1),
-   "A":(3, 2),
+    "7": (0, 0),
+    "8": (0, 1),
+    "9": (0, 2),
+    "4": (1, 0),
+    "5": (1, 1),
+    "6": (1, 2),
+    "1": (2, 0),
+    "2": (2, 1),
+    "3": (2, 2),
+    "": (3, 0),
+    "0": (3, 1),
+    "A": (3, 2),
 }
 
 
 dpad = {
-    "" : (0,0),
-    "^": (0,1),
-    "A": (0,2),
-    "<": (1,0),
-    "v": (1,1),
-    ">": (1,2),
+    "": (0, 0),
+    "^": (0, 1),
+    "A": (0, 2),
+    "<": (1, 0),
+    "v": (1, 1),
+    ">": (1, 2),
 }
 
 
 def solve_numpad(code):
-    current = "A"
-    result = ""
-    for c in code:
-        curr = num_pad[current]
-        nxt = num_pad[c]
-        vdiff = curr[0] - nxt[0]
-        hdiff = curr[1] - nxt[1]
-        if vdiff > 0:
-            result += "^"*vdiff
-        elif vdiff < 0:
-            result += "v"*abs(vdiff)
-        if hdiff > 0:
-            result += "<"*hdiff
-        elif hdiff < 0:
-            result += ">"*abs(hdiff)
-        result +="A"
-        current = c
-    return result
+    Q = deque([(num_pad["A"], 0, "")])
+    best = 100000000
+    results = []
+    while Q:
+        (r, c), n, route = Q.popleft()
+        if (r, c) == (3, 0):
+            continue
+        if n == len(code):
+            if len(route) < best:
+                best = len(route)
+                results = []
+            results.append(route)
+            continue
+        nr, nc = num_pad[code[n]]
+        if (r, c) == (nr, nc):
+            Q.append(((r, c), n + 1, route + "A"))
+            continue
+
+        if nr > r:
+            Q.append(((r + 1, c), n, route + "v"))
+        if nr < r:
+            Q.append(((r - 1, c), n, route + "^"))
+        if nc > c:
+            Q.append(((r, c + 1), n, route + ">"))
+        if nc < c:
+            Q.append(((r, c - 1), n, route + "<"))
+
+    return results
 
 
-def solve_dpad(code):
-    current = "A"
-    result = ""
-    for c in code:
-        curr = dpad[current]
-        nxt = dpad[c]
-        vdiff = curr[0] - nxt[0]
-        hdiff = curr[1] - nxt[1]
-        if vdiff > 0:
-            result += "^"*vdiff
-        elif vdiff < 0:
-            result += "v"*abs(vdiff)
-        if hdiff > 0:
-            result += "<"*hdiff
-        elif hdiff < 0:
-            result += ">"*abs(hdiff)
-        result +="A"
-        current = c
-    return result
+def solve_dpad(code, best=1000000000):
+    Q = deque([(dpad["A"], 0, "")])
+    results = []
+    while Q:
+        (r, c), n, route = Q.popleft()
+        if (r, c) == (0, 0):
+            continue
+        if len(route) > best:
+            continue
+        if n == len(code):
+            if len(route) < best:
+                best = len(route)
+                results = []
+            results.append(route)
+            continue
+        nr, nc = dpad[code[n]]
+        if (r, c) == (nr, nc):
+            Q.append(((r, c), n + 1, route + "A"))
+            continue
+
+        if nr > r:
+            Q.append(((r + 1, c), n, route + "v"))
+        if nr < r:
+            Q.append(((r - 1, c), n, route + "^"))
+        if nc > c:
+            Q.append(((r, c + 1), n, route + ">"))
+        if nc < c:
+            Q.append(((r, c - 1), n, route + "<"))
+
+    return results
 
 
 result = 0
 for line in lines:
+    print(line)
     ans = solve_numpad(line)
-    print(ans)
-    ans = solve_dpad(ans)
-    print(ans)
-    print(len(ans))
-    ans = solve_dpad(ans)
-    print(len(ans))
-    print(ans)
-    result += len(ans) * int(line.replace("A", ""))
+    uniq = set()
+    for a in ans:
+        temp = solve_dpad(a)
+        uniq = uniq.union(set(temp))
+    best = 10000000000000000000
+    for u in uniq:
+        temp = solve_dpad(u, best)
+        if temp:
+            best = min(best, len(temp[0]))
 
-# Part 1 =
+    result += best * int(line.replace("A", ""))
+    print(best)
+    print("=================")
+
+# Part 1 = 156714
 print(f"answer = {result}")
 
 result = 0
