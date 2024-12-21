86031928a7da008225a883e2455d640af6502725
Matt Clarke <matt.clarke@ess.eu>
2024-12-21 08:40:50 +0100

    Day 20: done

--- day_20/solver.py
+++ day_20/solver.py
@@ -45,49 +45,66 @@ def find_ref(track):
     assert False, "no solution"
 
 
-ref = find_ref(TRACK)
+def pprint(seen):
+    for r in range(len(lines)):
+        line = []
+        for c in range(len(lines[0])):
+            if TRACK[r, c] != "#" and (r, c) in seen:
+                line.append(str(seen[r, c]))
+            else:
+                line.append(TRACK[r, c])
+        print("".join(line))
+    input()
 
 
 def solve(num_cheat, min_diff=0):
-    results = []
+    results = {}
     for (r, c), current in ref.items():
         Q = deque([((r, c), 0)])
+        seen = {}
         while Q:
             (rr, cc), n = Q.popleft()
             if n > num_cheat:
                 continue
-            if TRACK.get((rr, cc)) == ".":
-                prev = ref[rr, cc]
-                if prev > current:
-                    diff = prev - current - n
-                    if diff > min_diff - 1:
-                        results.append(diff - n)
+            if (rr, cc) not in TRACK:
+                continue
+            if n >= seen.get((rr, cc), 1000000000):
+                continue
+            seen[(rr, cc)] = n
 
-            # Look in all four directions + 1 to see if we can hit a short cut
             for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
                 nr, nc = rr + dr, cc + dc
-                if n == 0 and TRACK.get((nr, nc)) != "#":
-                    continue
                 Q.append(((nr, nc), n + 1))
-                # if TRACK.get((nr, nc)) != "#":
-                #     continue
-                # for i in range(1, num_cheat):
-                #     nnr = nr + dr * i
-                #     nnc = nc + dc * i
-                #     if TRACK.get((nnr, nnc)) == ".":
-                #         prev = ref[nnr, nnc]
-                #         if prev > current:
-                #             diff = prev - current
-                #             results.append(diff - 2)
+        # pprint(seen)
+        for (rr, cc), n in seen.items():
+            if TRACK[rr, cc] == "#":
+                continue
+            if (rr, cc) == (r, c):
+                continue
+            value = ref[rr, cc]
+            curr = ref[r, c]
+            if value < curr:
+                continue
+            if curr + n >= value:
+                continue
+            savings = value - current - n
+            if savings < min_diff:
+                continue
+            results[savings] = results.get(savings, 0) + 1
+
     return results
 
 
-result = len(solve(2, 100))
+ref = find_ref(TRACK)
+
+results = solve(2, 100)
+result = sum(results.values())
 
 # Part 1 = 1381
 print(f"answer = {result}")
 
-result = 0
+results = solve(20, 100)
+result = sum(results.values())
 
-# Part 2 =
+# Part 2 = 982124
 print(f"answer = {result}")
