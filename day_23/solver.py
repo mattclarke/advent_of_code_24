cad8647a978ee90c5dd50dc5fd2efd692628f480
Matt Clarke <matt.clarke@ess.eu>
2024-12-23 07:08:16 +0100

    day 23: part 1

--- day_23/solver.py
+++ day_23/solver.py
@@ -1,6 +1,6 @@
 import copy
 import sys
-from collections import deque
+from collections import deque, defaultdict
 
 
 FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
@@ -9,17 +9,38 @@ with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
 
 result = 0
+
+layout = defaultdict(set)
+tees = set()
 
 for line in lines:
-    pass
+    a, b = line.split("-")
+    layout[a].add(b)
+    layout[b].add(a)
+    if a.startswith("t"):
+        tees.add(a)
+    if b.startswith("t"):
+        tees.add(b)
 
-# Part 1 = 
-print(f"answer = {result}")
+solutions = set()
+
+for t in tees:
+    nodes = layout[t]
+    for n in nodes:
+        nodes2 = layout[n]
+        for nn in nodes2:
+            if n == nn or nn == t:
+                continue
+            if t in layout[nn]:
+                solutions.add(frozenset({t, n, nn}))
+
+
+# Part 1 = 1218
+print(f"answer = {len(solutions)}")
 
 result = 0
 
-# Part 2 = 
+# Part 2 =
 print(f"answer = {result}")
