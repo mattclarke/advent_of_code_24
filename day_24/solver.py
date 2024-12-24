e81404306055f19aea3c0364594ecdf03cdeac0f
Matt Clarke <matt.clarke@ess.eu>
2024-12-24 08:46:28 +0100

    day 24: part 2 wip

--- day_24/solver.py
+++ day_24/solver.py
@@ -13,13 +13,21 @@ initial, lines = [line.strip() for line in PUZZLE_INPUT.split("\n\n") if line]
 VALUES = {}
 for i in initial.split("\n"):
     n, v = i.split(": ")
+    n = n.replace("x0", "x").replace("y0", "y")
     VALUES[n] = int(v)
 
 GATES = []
 for i in lines.split("\n"):
-    GATES.append(tuple(i.replace("-> ", "").split(" ")))
+    GATES.append(
+        tuple(
+            i.replace("-> ", "")
+            .replace("x0", "x")
+            .replace("y0", "y")
+            .replace("z0", "z")
+            .split(" ")
+        )
+    )
 
-result = 0
 values = copy.copy(VALUES)
 gates = deque(GATES[:])
 
@@ -40,12 +48,8 @@ while gates:
 zeds = []
 for i in range(100):
     z = "z"
-    if i < 10:
-        z += "0" + str(i)
-    else:
-        z += str(i)
+    z += str(i)
     if z not in values:
-        print(f"max z is {z}")
         break
     zeds.append(str(values[z]))
 
@@ -56,7 +60,27 @@ result = int(zedstr, 2)
 # Part 1 = 45923082839246
 print(f"answer = {result}")
 
-result = 0
+
+def solve(values, gates, x, y):
+    x = f"{x:045b}"
+    y = f"{y:044b}"
+    for i, xx, yy in enumerate(zip(reversed(x), reversed(y))):
+        pass
+
+    while gates:
+        a, cmd, b, c = gates.popleft()
+        if a in values and b in values:
+            if cmd == "OR":
+                values[c] = values[a] | values[b]
+            elif cmd == "AND":
+                values[c] = values[a] & values[b]
+            elif cmd == "XOR":
+                values[c] = values[a] ^ values[b]
+            else:
+                assert False
+        else:
+            gates.append((a, cmd, b, c))
+
 
 # Part 2 =
 print(f"answer = {result}")
